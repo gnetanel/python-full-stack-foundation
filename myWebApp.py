3fcbc93c688c7bc0a36a2bd10d57535da4b880cc
netanel <gal.netanel.mobile@gmail.com>
2018-08-15 09:24:23 +0300

    add idea files

--- myWebApp.py
+++ myWebApp.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 from database_setup import engine, Base
+from flask import Flask
 
 Base.metadata.bind=engine
 DBSession = sessionmaker(bind=engine)
