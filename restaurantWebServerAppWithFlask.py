08568c1a4a8a9144039610d61dd280fb718e3076
netanel <gal.netanel.mobile@gmail.com>
2018-08-16 08:47:16 +0300

    Add more templdates filed for add edit menu items

--- restaurantWebServerAppWithFlask.py
+++ restaurantWebServerAppWithFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 # import cgi
@@ -15,65 +15,64 @@ DBSession = sessionmaker(bind=engine)
 app = Flask(__name__)
 
 
-# stopped at https://classroom.udacity.com/courses/ud088/lessons/3593308717/concepts/36245586140923
+# stopped at https://classroom.udacity.com/courses/ud088/createMenuItemlessons/3593308717/concepts/36245586140923
 
 
 @app.route("/")
-@app.route("/index")
-def printall():
+@app.route("/restaurants")
+def restaurantsIndex():
     session = DBSession()
-    restaurant = session.query(Restaurant).first()
-    menuItems = session.query(MenuItem).filter_by(restaurantId=restaurant.id)
-
-    output = ""
-    output += "<h1>Restaurant " + restaurant.name + "</h1>"
-    output += "</br>Menu</br>"
-
-    for menuItem in menuItems:
-        output += menuItem.name + ", " + menuItem.course + ", " + menuItem.description + ", " + menuItem.price
-        output += "</br>"
-
-    return render_template("menu.html", restaurant=restaurant, items=menuItems)
-    # return output
+    restaurants = session.query(Restaurant)
+    return render_template('restaurantsIndex.html', restaurants=restaurants)
 
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/new')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurants(restaurant_id):
+    session = DBSession()
+    print("Query restaurant db")
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).first()
+    print("Query Menu db")
+    items = session.query(MenuItem).filter_by(restaurantId=restaurant.id)
+    return render_template("menu.html", restaurant=restaurant, items=items)
+
+
+@app.route('/restaurant/<int:restaurant_id>/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    output = ""
-    output += "<form method='post' action=''>" \
-              "<br>Name:" \
-              " <input type='text' name='name'>" \
-              "<br>Description:" \
-              " <input type='text' name='description'>" \
-              "<br>Price:" \
-              " <input type='text' name='price'>" \
-              "<br>Course:" \
-              " <input type='text' name='course'>" \
-              "<br>" \
-              " <input type='submit' value='Submit'>" \
-              "</form>"
-    return output
+    session = DBSession()
+    if request.method == "GET":
+        return render_template('newMenuItem.html', )
+    if request.method == "POST":
+        menuItem = MenuItem(name=request.form['name'], description=request.form['description'],
+                            price=request.form['price'], course=request.form['course'], restaurantId=restaurant_id)
+        session.add(menuItem)
+        session.commit()
+        return redirect(url_for('restaurants', restaurant_id=restaurant_id))
 
 
 # Task 2: Create route for editMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    output = ""
-    output += "<form method='post' action=''>" \
-              "<br>Name:" \
-              " <input type='text' name='name'>" \
-              "<br>Description:" \
-              " <input type='text' name='description'>" \
-              "<br>Price:" \
-              " <input type='text' name='price'>" \
-              "<br>Course:" \
-              " <input type='text' name='course'>" \
-              "<br>" \
-              " <input type='submit' value='Submit'>" \
-              "</form>"
-    return output
+    session = DBSession()
+    if request.method == "GET":
+        restaurant = session.query(Restaurant).filter_by(id=restaurant_id).first()
+        menuItem = session.query(MenuItem).filter_by(id=menu_id).first()
+        return render_template('editMenuItem.html', menuItem=menuItem, restaurant=restaurant)
+    if request.method == "POST":
+        menuItem = session.query(MenuItem).filter_by(id=menu_id).first()
+        if request.form['name']:
+            menuItem.name = request.form['name']
+        if request.form['description']:
+            menuItem.description = request.form['description']
+        if request.form['course']:
+            menuItem.course = request.form['course']
+        if request.form['price']:
+            menuItem.price = request.form['price']
+
+        session.add(menuItem)
+        session.commit()
+        return redirect(url_for('restaurants', restaurant_id=restaurant_id))
 
 
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
